0f8b93679d7d5157c9704ddd4163349f6a5e054d
Thomas Leese <thomas@leese.io>
2021-01-10 16:35:29 +0000

    Create LibGDX project

---
+++ desktop/src/com/orycion/blockworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.orycion.blockworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.orycion.blockworld.BlockWorldGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockWorldGame(), config);
+	}
+}
