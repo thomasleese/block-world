a9c84b74f8671c1c76b0822e09aec0321a753306
Thomas Leese <thomas@leese.io>
2021-01-11 10:20:40 +0000

    Render first level

--- core/src/com/orycion/blockworld/BlockWorldGame.kt
+++ core/src/com/orycion/blockworld/BlockWorldGame.kt
@@ -3,28 +3,49 @@ package com.orycion.blockworld
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 
 class BlockWorldGame : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+    private val assetManager = AssetManager()
+    private val camera = OrthographicCamera()
+    private val viewport = ExtendViewport(20f, 20f, camera)
+    private lateinit var spriteBatch: SpriteBatch
+    private lateinit var mapRenderer: OrthogonalTiledMapRenderer
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("textures/tilesets/blocks.png")
+        assetManager.setLoader(TiledMap::class.java, TmxMapLoader(InternalFileHandleResolver()))
+        assetManager.load("levels/first.xml", TiledMap::class.java)
+        assetManager.finishLoading()
+
+        spriteBatch = SpriteBatch()
+
+        val map = assetManager.get<TiledMap>("levels/first.xml")
+        mapRenderer = OrthogonalTiledMapRenderer(map, 1 / 96f, spriteBatch)
     }
 
     override fun render() {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+
+        viewport.apply()
+
+        mapRenderer.setView(camera)
+        mapRenderer.render()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height)
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+        mapRenderer.dispose()
+        assetManager.dispose()
     }
 }
